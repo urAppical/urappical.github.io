5a9954dabfbbcf7a161ea74ac2222e53891dc6e0
matveshok <103825755+urAppical@users.noreply.github.com>
2022-04-20 18:08:57 +0700

    1

--- scripts/cat-script.js
+++ scripts/cat-script.js
@@ -1,5 +1,7 @@
-const cat = document.getElementById("cat")
+var cat = document.getElementById("cat")
 
-cat.addEventListener("click", function(e){
-    e.setAttribute("src", "images/gru-with-gun.png")
+// you cant pass paramenter to anonymos functuion, only declare in bracets
+cat.addEventListener("click", function(){
+    //alert("aaaaaaaaaaa")
+    cat.setAttribute("src", "images/gru-with-gun.png")
 })

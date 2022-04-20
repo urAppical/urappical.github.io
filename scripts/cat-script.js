8f460010ccb6d34ca419b72f44f6d878f8caa58a
matveshok <103825755+urAppical@users.noreply.github.com>
2022-04-20 17:47:30 +0700

    1

--- scripts/cat-script.js
+++ scripts/cat-script.js
@@ -1,3 +1,4 @@
 const image = document.querySelector("img")
-
-image.setAttribute("src", "images/gru-with-gun.png")
+image.onclick = function(){
+    image.setAttribute("src", "images/gru-with-gun.png")
+}

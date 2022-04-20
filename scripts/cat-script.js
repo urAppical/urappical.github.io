ecd8cdcc214443afe4e5d6f9f805fbcd7113e39d
matveshok <103825755+urAppical@users.noreply.github.com>
2022-04-20 18:30:46 +0700

    secret added

--- scripts/cat-script.js
+++ scripts/cat-script.js
@@ -1,7 +1,20 @@
+// dont forget to use google developer tools (f12) for debugging
 var cat = document.getElementById("cat")
 
 // you cant pass paramenter to anonymos functuion, only declare in bracets
 cat.addEventListener("click", function(){
     //alert("aaaaaaaaaaa")
-    cat.setAttribute("src", "images/gru-with-gun.png")
+    if (!document.querySelector("#cat-note")){
+        
+        alert("Gotcha!")
+
+        cat.setAttribute("src", "images/gru-with-gun.png")
+        const parag = document.createElement("p")
+        parag.setAttribute("id", "cat-note")
+        const conc = document.querySelector("section.conclusion")
+        parag.textContent = "Don't you dare to touch my cat! (it's not mine actually)."
+        parag.setAttribute("class", "comment")
+        conc.appendChild(parag)
+    }
 })
+
